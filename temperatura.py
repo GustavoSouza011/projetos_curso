01afe8e6ed99fdc466c4aac59d88a0202aa4a953
beiçola__gozado <gsgustavosouza011@gmail.com>
2025-06-11 10:13:44 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,10 @@
+nome = str (input("digite seu nome"))
+temperatura = float (input("qual é sua temperatura"))
+if 20 <= temperatura <= 40 :
+    print (f"(nome)slk onde tem ventilador ?")
+elif 20> temperatura >= 5:
+    print(f"(nome)friozinho né ?")
+elif 0 < temperatura <=5:
+    print(f"(nome)calmo gelado kkkkk frio do krl")
+else :
+    print (f"(nome)morri já kkkkkk")
