ae1a00c0221fd6ac2fb8eae21362a43b9ab9da84
beiçola__gozado.com <gsgustavosouza011@gmail.com>
2025-07-30 10:10:56 -0300

    Add files via upload

---
+++ sla.py
@@ -0,0 +1,20 @@
+pergunta = input ("oq vc quer fazer, aperte enter para prosseguir")
+
+opção = int(input(" escolha algum numero para oq vc quer fzr : \n 1- virar clt \n 2 - odeio trabalhar \n 3 - influencer \n 4 - streamer"))
+
+def virar_clt ():
+    print("ganhe pouco e trabalhe muito kkkk")
+def odeio_trampar ():
+    print ("vai trabalha e ajuda sua mãe vagabundo !!")
+def influencer_rico ():
+    print ("vai ter inveja do nino e do feijuca, e vai conquistar nd kakak")
+def streamer_pobre ():
+    print ("nunca vai conseguir, só regresso rsrs")
+if opção == 1:
+    virar_clt ()
+elif opção == 2:
+    odeio_trampar ()
+elif opção == 3:
+    influencer_rico ()
+elif opção == 4:
+    streamer_pobre ()
